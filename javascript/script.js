8d6984b655986bbcf113f5aaa7d4f2a06818352d
Roberto Lozano <rlozano@ucdavis.edu>
2021-03-16 15:28:58 -0700

    added animations

--- javascript/script.js
+++ javascript/script.js
@@ -1,65 +1,21 @@
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    var day = today.getDate();
-    var month = today.getMonth();
-    var textMonth;
-    var year = today.getFullYear();
-    var ampm = "am";
-    var hour;
+var me_section_left = document.getElementById("me_section_left");
+var me_section_right = document.getElementById("me_section_right");
+var me_section_right = document.getElementById("me_section_right");
 
-    textMonth = translateMonth(month);
-    ampm = timeOfDay(h);
-    m = checkTime(m);
-    s = checkTime(s);
-    hour = militaryToStandard(h);
-    document.getElementById('txt').innerHTML =
-        hour + ":" + m + ":" + s + " " + ampm + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0" + day + " " + textMonth + " " + year;
+ScrollReveal().reveal(me_section_left,{
+    origin: 'left',
+    distance: '50%',
+    duration: 820
+});
 
-    var t = setTimeout(startTime, 500);
-}
+ScrollReveal().reveal(me_section_right,{
+    origin: 'right',
+    distance: '50%',
+    duration: 820
+});
 
- function translateMonth(i){
-    if(i == 0){return "January";}
-    if(i == 1){return "February";}
-    if(i == 2){return "March";}
-    if(i == 3){return "April";}
-    if(i == 4){return "May";}
-    if(i == 5){return "June";}
-    if(i == 6){return "July";}
-    if(i == 7){return "August";}
-    if(i == 8){return "September";}
-    if(i == 9){return "October";}
-    if(i == 10){return "November";}
-    if(i == 11){return "December";}
-    else{
-        return "error";
-    }
-}
-
-function timeOfDay(i){
-    if(i >= 12)
-    {
-        return "PM";
-    }
-    else{
-        return "AM";
-    }
-}
-
-function militaryToStandard(i){
-    if(i > 12)
-    {
-        return i-12;
-    }
-    else{
-        return i;
-    }
-}
-
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
+ScrollReveal().reveal(project,{
+    origin: 'right',
+    distance: '50%',
+    duration: 1500
+});
