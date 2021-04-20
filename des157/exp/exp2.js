3f7d0b053d9e0afcb525ffeffceca0793c137487
Roberto Lozano <rlozano@ucdavis.edu>
2021-04-20 10:26:14 -0700

    exp2 capitalize

--- des157/exp/exp2.js
+++ des157/exp/exp2.js
@@ -5,10 +5,10 @@ let colorbtn = document.getElementById('changecolor');
 
 btn.addEventListener('click', function(){
     let paragraph = prompt("Please enter a paragraph to enter");
-
     if(paragraph == null){
         return;
     }
+    paragraph = capitalizeFirstLetter(paragraph);
 
     let newP = document.createElement('p');
     let pText = document.createTextNode(paragraph);
@@ -20,3 +20,9 @@ colorbtn.addEventListener('click', function(){
     let color = prompt("Please enter a color for text");
     divTag.style.color=color;
 });
+
+function capitalizeFirstLetter(paragraph){
+    newParagraph = paragraph.charAt(0).toUpperCase() + paragraph.slice(1);
+    console.log(newParagraph);
+    return newParagraph;
+}
