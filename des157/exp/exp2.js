d32b90b9c3ba6ea26cd797e43db859abe414bcc5
Roberto Lozano <rlozano@ucdavis.edu>
2021-04-20 10:19:04 -0700

    adding null case exp2

--- des157/exp/exp2.js
+++ des157/exp/exp2.js
@@ -2,8 +2,13 @@ let divTag = document.querySelector('div');
 let btn = document.getElementById('addp');
 let colorbtn = document.getElementById('changecolor');
 
+
 btn.addEventListener('click', function(){
-    var paragraph = prompt("Please enter a paragraph to enter");
+    let paragraph = prompt("Please enter a paragraph to enter");
+
+    if(paragraph == null){
+        return;
+    }
 
     let newP = document.createElement('p');
     let pText = document.createTextNode(paragraph);
@@ -12,6 +17,6 @@ btn.addEventListener('click', function(){
 });
 
 colorbtn.addEventListener('click', function(){
-    var color = prompt("Please enter a color for text");
+    let color = prompt("Please enter a color for text");
     divTag.style.color=color;
 });
